6dcbdfc9fa4f4651846c547706be2239eb46131f
ssalihah <46809944+ssalihah@users.noreply.github.com>
2020-03-28 13:57:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.debug = True
 
 # Step 03: add database configurations here
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://asm02_user:password@localhost:5432/asm02'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://asm02user:password@localhost:5432/asm02db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -22,9 +22,20 @@ def create_user():
 	print('create_user')
 
 	# start your code after this line
-	# get values from request.json 
-	name = request.json['name'] 
-	contact_number = request.json['contact'] 
+	if 'name' and 'contact' in request.json:
+		name = request.json['name'] 
+		contact_number = request.json['contact'] 
+	else:
+		return "Please ensure you have written the field names correctly"
+
+	if isinstance(name,str)==False:
+		return "Please ensure your name is in the string format"
+
+	if name=="":
+		return "Please ensure your name is filled up"
+
+	if isinstance(contact_number,int)==False:
+		return "Please ensure your contact is in integer"
 
 	try:
 		new_user = User(name=name, contact_number=contact_number)
@@ -33,11 +44,11 @@ def create_user():
 		if len(str(contact_number))==8:
 			return jsonify('new user {}'.format(new_user))
 		else:
-			return "Please type an 8 digit number" 
+			return "Please type an 8 digit number"
 		
-
 	except Exception as e:
 		return (str(e))
+
 	# end your code before this line
 
 @app.route('/temp/', methods=['POST']) 
@@ -57,22 +68,51 @@ def create_temp():
 	if name=="":
 		return "Please ensure your friend's name is filled up."
 
-	check_name = User.query.filter_by(name=name).first()
-
-	if check_name is None:
-		return jsonify('user {} does not exist'.format(name))
-	
-	
-	try: #select user_id  where name = name
-		user_id = db.session.query(User.id).filter(User.name==name)
-		# User.query(User.id).filter(User.name == name).first()
-		if user_id != None:
-			new_temp = Temperature(user_id=user_id, temp_value=temp)
+	try:
+		searched_user = User.query.filter_by(name=name).first()
+		if searched_user != None:
+			searched_user_id = searched_user.id		
+			new_temp = Temperature(temp_value=temp_value, user_id=searched_user_id)
 			db.session.add(new_temp)
 			db.session.commit()
 			return jsonify('new temp record {} was created for user {}'.format(new_temp,name))
 		else:
 			return "Are you sure {} is your friend?".format(name)
+
+		
+	except Exception as e:
+		return (str(e))
+	# end your code before this line
+
+@app.route('/friend/', methods=['PUT']) 
+def update_friend():
+	print('update_friend')
+
+	# start your code after this line
+
+	# end your code before this line
+
+@app.route('/user/', methods=['GET']) 
+def get_user():
+	print('get_user')
+
+	# start your code after this line
+
+	# end your code before this line
+
+@app.route('/temp/', methods=['GET']) 
+def get_temp():
+	print('get_temp')
+
+	# start your code after this line
+	temperatures = Temperature.query.all()
+	return jsonify([t.serialize() for t in temperatures])	
+	# end your code before this line
+
+# your code ends here 
+
+if __name__ == '__main__':
+	app.run(debug=True)
 
 
 		# this must be done before adding reviews
