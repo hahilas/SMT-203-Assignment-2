10518254c9ffcff184a5715d9e67ff2e3e9c1dd8
lloisll <46128773+lloisll@users.noreply.github.com>
2020-03-30 00:05:32 +0800

    Update models.py

--- models.py
+++ models.py
@@ -2,11 +2,6 @@ import datetime
 
 from app import db
 
-friendship_table = db.Table('friendship',
-db.Column('friend_from', db.Integer, db.ForeignKey('user.id'),primary_key=True),
-db.Column('friend_to', db.Integer, db.ForeignKey('user.id'),
-primary_key=True))
-
 class User(db.Model):
 	__tablename__ = 'user'
 
@@ -15,25 +10,20 @@ class User(db.Model):
 	name = db.Column(db.String(80), unique=True, nullable=False)
 	contact_number = db.Column(db.Integer, unique=True, nullable=False)
 	
-	#one-to-many model btw user and temperature
+	#one-to-many model
 	temperatures = db.relationship('Temperature', back_populates='user', uselist= True, cascade='all,delete-orphan', lazy=True)
 
-	# many-to-many model to the same class
-	friends = db.relationship('User', secondary=friendship_table,
-	primaryjoin=id == friendship_table.c.friend_to,
-	secondaryjoin=id==friendship_table.c.friend_from)
 	# end your code before this line
 
-	def __init__(self, name, contact_number,temperatures=None, friends = None):
+	def __init__(self, name, contact_number,temperatures=None):
 		# start your code after this line
 		self.name = name
 		self.contact_number = contact_number
 		self.temperatures = [] if temperatures is None else temperatures
-		self.friends = friends
 		# end your code before this line
 
-	# def __repr__(self):
-	# 	return '{} was created with id {}'.format(self.name, self.id) 
+	def __repr__(self):
+		return '{} was created with id {}'.format(self.name, self.id) 
 		
 	
 	def serialize(self):
@@ -41,20 +31,12 @@ class User(db.Model):
 		#querying all the temp records of a particular user
 		user_temp_data = Temperature.query.filter_by(user_id = self.id).all()
 
-		# return {
-		# 	'contact_number': self.contact_number,
-		# 	'id':self.id,
-		# 	'name': self.name, 
-		# 	'temp_logs':[] if self.temperatures == [] else [{"temp":temp_entry.temp_value,"timestamp":temp_entry.timestamp} for temp_entry in user_temp_data]		
-		# }
 		return {
 			'contact_number': self.contact_number,
-			'friends': self.friends,
 			'id':self.id,
 			'name': self.name, 
 			'temp_logs':[] if self.temperatures == [] else [{"temp":temp_entry.temp_value,"timestamp":temp_entry.timestamp} for temp_entry in user_temp_data]		
 		}
-
 		# end your code before this line
 
 class Temperature(db.Model):
@@ -89,3 +71,5 @@ class Temperature(db.Model):
 			'user_name':User.query.filter_by(id=self.user_id).first().name
 		}
 		# end your code before this line
+
+
